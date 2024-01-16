5bfa39a92ac4b76842ba8f8b48882f546245246d
Rakesh Kumar Nayak <128237968+royalguy15@users.noreply.github.com>
2024-01-16 23:20:10 +0530

    Add files via upload

---
+++ flo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class flo {
+    public static void mian(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        float c = a + b;
+
+        System.out.println(c);
+    }
+
+}
